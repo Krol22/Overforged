b185144522f3409b3ae637a9b0f5ce0507f1f089
Karol T <krol22ee@gmail.com>
2023-08-14 09:24:17 +0200

    Adding ecs

---
+++ src/core/ecs.ts
@@ -0,0 +1,96 @@
+class Ecs {
+  private isRunning = false;
+
+  private entities: Entity[] = [];
+  private systems: System[] = [];
+
+  private entitiesToRemove: Array<string> = [];
+
+  addEntities(entities: Entity[]) {
+    this.entities.push(...entities);
+
+    if (this.isRunning) {
+      this.updateSystemEntities();
+    }
+  }
+
+  removeEntities(ids: Array<string>) {
+    if (this.isRunning) {
+      this.entitiesToRemove = ids;
+      return;
+    }
+
+    this.entities = this.entities.filter((entity) => ids.includes(entity.id));
+  }
+
+  addSystems(systems: System[]) {
+    // Systems will be added only on start
+    this.systems.push(...systems);
+  }
+
+  start() {
+    this.isRunning = true;
+    this.updateSystemEntities();
+  }
+
+  updateSystemEntities() {
+    for (const system of this.systems) {
+      const systemEntities = this.entities.filter((entity) => entity.hasEvery(system.componentTypes));
+      system.setSystemEntities(systemEntities);
+    }
+  }
+
+  update(dt: number) {
+    for (const system of this.systems) {
+      system.update(dt);
+    }
+
+    if (this.entitiesToRemove.length > 0) {
+      this.entities = this.entities.filter((entity) => this.entitiesToRemove.includes(entity.id));
+      this.entitiesToRemove = [];
+      this.updateSystemEntities();
+    }
+  }
+}
+
+abstract class Component {
+  public type: string;
+
+  constructor(type: string) {
+    this.type = type;
+  }
+}
+
+class Entity {
+  public id: string;
+  private components: Record<string, Component> = {};
+
+  constructor() {
+    this.id = '1';
+  }
+
+  addComponents(components: Component[]) {
+    components.forEach((component) => {
+      this.components[component.type] = component;
+    });
+  }
+
+  hasEvery(types: Array<string>) {
+    return types.every((type) => !!this.components[type]);
+  }
+
+  getComponent(type: string) {
+    return this.components[type];
+  }
+}
+
+abstract class System {
+  public componentTypes: Array<string> = [];
+  private systemEntities: Array<Entity> = [];
+
+  public setSystemEntities(systemEntities: Entity[]) {
+    this.systemEntities = systemEntities;
+  }
+
+  public abstract update(dt: number) {}
+}
